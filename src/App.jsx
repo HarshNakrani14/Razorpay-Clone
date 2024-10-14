6dc5ba1a228cdbba33a7cd7c501eeb5362266dc7
Harsh Nakrani <nakraniharsh1412@gmail.com>
2024-10-14 17:47:05 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import HomePage from './Pages/HomePage';
 import Cards from './Components/Card1';
 
 function App() {
   return (
-    <div className="font-Poppins bg-gradient-to-r from-blue-50 to-blue-100">
-      <Navbar />
-      <HomePage />
-       
-    </div>
+    <Router>
+      <div className="font-Poppins bg-gradient-to-r from-blue-50 to-blue-100">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/cards" component={Cards} />
+          {/* Add more routes as needed */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
